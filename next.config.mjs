5a366fdbe3a3654454c197769c5d8256f7914864
Benjy <123623302+brrock@users.noreply.github.com>
2024-12-01 13:36:01 +0000

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 }
 
